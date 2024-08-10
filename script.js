6a999ab2e591a709c74bd95c64df53060cfd2b32
Jillian Chong <jc2886@cornell.edu>
2024-08-10 03:06:04 -0400

    Added footer

---
+++ script.js
@@ -0,0 +1,8 @@
+// Home page project card redirects
+
+console.log("working");
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("subwaydle-card").addEventListener("click", function() {
+        window.location.href = "project-pages/subwaydle.html";
+    });
+});
