a8fdaed10edc892909f3a65a3121f6a47ee87a7e
Jillian Chong <jc2886@cornell.edu>
2024-08-10 19:30:11 -0400

    Contact footer added, project page created

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Home page project card redirects
 
 console.log("working");
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById("subwaydle-card").addEventListener("click", function() {
-        window.location.href = "project-pages/subwaydle.html";
-    });
-});
+
+window.onload = function() {
+    var subwaydleCard = document.getElementById("subwaydle-card");
+    if (subwaydleCard) {
+        subwaydleCard.addEventListener("click", function() {
+            window.location.href = "/project-pages/subwaydle.html";
+        });
+    }
+}
