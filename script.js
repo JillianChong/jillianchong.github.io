e84ee844e1d1ebcb768ebfbd4fc9e58657504887
Jillian Chong <jc2886@cornell.edu>
2024-08-12 23:41:31 -0400

    Redirect buttons created for code and play

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 // Home page project card redirects
 
-console.log("working");
+cards = ["subwaydle-card", "todolist-card", "superhamster-card", "genetics-card"]
+hrefs = ["subwaydle.html", "todolist.html", "superhamster.html", "genetics.html"]
+
+project_code = ["subwaydle-github"]
+project_code_hrefs = ["https://github.com/JillianChong/Subwaydle"]
+project_play = ["subwaydle-game"]
+project_play_hrefs = ["https://subwaydle.onrender.com/"]
 
 window.onload = function() {
-    var subwaydleCard = document.getElementById("subwaydle-card");
-    if (subwaydleCard) {
-        subwaydleCard.addEventListener("click", function() {
-            window.location.href = "/project-pages/subwaydle.html";
-        });
+    for(var i = 0; i < cards.length; i++) {
+        (function(index) {
+            var card = document.getElementById(cards[index]);
+            if (card) {
+                card.addEventListener("click", function() {
+                    window.location.href = "/project-pages/" + hrefs[index];
+                });
+            } else {
+                console.log("Card not found.")
+            }
+        })(i);
+    }
+
+    for(var i = 0; i < project_code.length; i++) {
+        (function(index) {
+            var code = document.getElementById(project_code[index]);
+            if (code) {
+                code.addEventListener("click", function() {
+                    window.open(
+                        project_code_hrefs[index], "_blank"
+                    );
+                });
+            } else {
+                console.log("Index not found.")
+            }
+
+            var play = document.getElementById(project_play[index]);
+            if (play) {
+                play.addEventListener("click", function() {
+                    window.open(
+                        project_play_hrefs[index], "_blank"
+                    );
+                });
+            } else {
+                console.log("Index not found.")
+            }            
+        })(i);
     }
 }
